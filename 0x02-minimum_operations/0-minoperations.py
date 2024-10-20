34662160c0511ba75b059120a6fdc02d83a5e579
SuzanFah <susiefahim@gmail.com>
2024-10-20 21:00:00 +0000

    correcting algorithm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,35 @@
-def minOperations(n):
-    if n <= 1:
-        return 0
-    
-    operations = 0
-    divisor = 2
-    
-    # Keep dividing n by the smallest divisor
-    while n > 1:
-        # While n is divisible by the current divisor
-        while n % divisor == 0:
-            # Add the divisor to the operation count (this represents Copy + Paste operations)
-            operations += divisor
-            # Reduce n by dividing it by the current divisor
-            n //= divisor
-        # Move to the next divisor
-        divisor += 1
-    
-    return operations
+#!/usr/bin/python3
+'''The minimum operations coding challenge.
+'''
 
+
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
