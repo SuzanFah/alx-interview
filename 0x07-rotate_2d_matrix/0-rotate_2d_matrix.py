50644023103ffb17f8ff1bf0d3fc3e8976e34ed2
SuzanFah <susiefahim@gmail.com>
2024-11-20 04:48:18 +0200

    Commit all

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module for rotating a 2D matrix 90 degrees clockwise."""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotates a 2D matrix 90 degrees clockwise in-place.
+    Args:
+        matrix (list[list]): The 2D matrix to rotate
+    """
+    n = len(matrix)
+    
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
