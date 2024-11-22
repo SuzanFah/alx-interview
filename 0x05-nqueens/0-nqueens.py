bb429dad452c0673ad4890a30119e0017d8aa8bc
SuzanFah <susiefahim@gmail.com>
2024-11-22 12:02:53 +0200

    n

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,76 @@
+
+#!/usr/bin/python3
+"""N Queens placement solution"""
+import sys
+
+
+def is_safe(board, row, col, n):
+    """Check if a queen can be placed on board[row][col]"""
+    # Check this row on left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on left side
+    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens(board, col, n, solutions):
+    """Recursive function to solve N Queens problem"""
+    if col >= n:
+        solution = []
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == 1:
+                    solution.append([i, j])
+        solutions.append(solution)
+        return
+
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_nqueens(board, col + 1, n, solutions)
+            board[i][col] = 0
+
+
+def print_solutions(n):
+    """Main function to print solutions"""
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    # Initialize board
+    board = [[0 for x in range(n)] for y in range(n)]
+    solutions = []
+
+    solve_nqueens(board, 0, n, solutions)
+
+    # Print solutions
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+        print_solutions(n)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
